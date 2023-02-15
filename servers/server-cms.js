8aa3227964918d87ccadce13f6bcb9171cee3595
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-15 23:00:08 +0100

    updated write model with better json structure

--- servers/server-cms.js
+++ servers/server-cms.js
@@ -5,32 +5,58 @@ const cms = express();
 
 cms.use(express.json());
 cms.use(express.urlencoded({ extended: true }));
-cms.use("/cms", express.static(path.join(__dirname, "/../../../node_modules/cosdb-client-framework/cms")));
-cms.use("/core", express.static(path.join(__dirname, "/../../../node_modules/cosdb-client-framework/core")));
-cms.use("/components", express.static(path.join(__dirname, "/../../../node_modules/cosdb-client-framework/components")));
+cms.use(
+  "/cms",
+  express.static(
+    path.join(__dirname, "/../../../node_modules/cosdb-client-framework/cms")
+  )
+);
+cms.use(
+  "/core",
+  express.static(
+    path.join(__dirname, "/../../../node_modules/cosdb-client-framework/core")
+  )
+);
+cms.use(
+  "/components",
+  express.static(
+    path.join(
+      __dirname,
+      "/../../../node_modules/cosdb-client-framework/components"
+    )
+  )
+);
 
 cms.get("/read", (req, res) => {
-  fs.readFile(path.join(__dirname, "/../../../model.json"), "utf-8", (error, data) => {
-    if (error) {  
-      console.error("An error occurred while reading the file:", error);
-      res.sendStatus(500);
-    } else {
-      res.json(JSON.parse(data));
+  fs.readFile(
+    path.join(__dirname, "/../../../model.json"),
+    "utf-8",
+    (error, data) => {
+      if (error) {
+        console.error("An error occurred while reading the file:", error);
+        res.sendStatus(500);
+      } else {
+        res.json(JSON.parse(data));
+      }
     }
-  });
+  );
 });
 
 cms.put("/update", (req, res) => {
   const data = req.body;
-  fs.writeFile(path.join(__dirname, "/../../../model.json"), JSON.stringify(data), (error) => {
-    if (error) {
-      res
-        .status(500)
-        .send({ message: "An error occurred while saving the file." });
-    } else {
-      res.send({ message: "The file has been successfully updated." });
+  fs.writeFile(
+    path.join(__dirname, "/../../../model.json"),
+    JSON.stringify(data, null, 4),
+    (error) => {
+      if (error) {
+        res
+          .status(500)
+          .send({ message: "An error occurred while saving the file." });
+      } else {
+        res.send({ message: "The file has been successfully updated." });
+      }
     }
-  });
+  );
 });
 
 cms.listen(3001, () => {
