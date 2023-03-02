68d70644f5feda8f412bb10de5454dcda008a829
Maja Frankiewicz <majafrank@live.se>
2023-03-02 16:19:53 +0100

    auto commit to dev2

--- servers/server-app.js
+++ servers/server-app.js
@@ -1,19 +1,22 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const app = express();
 
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// app.use(express.static(path.join(__dirname, "/app")));
 
-app.use(
-  "/",
-  express.static(
-    path.join(__dirname, "/../../../node_modules/cosdb-client-framework/app-boilerplate")
-  )
-);
+app.get('*', function(req, res, next) {
+    res.sendFile(path.join(__dirname, "/../../../node_modules/cosdb-client-framework/app-boilerplate/index.html"), { root: 'dist' });
+})
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
 });
+
+module.exports = app;
