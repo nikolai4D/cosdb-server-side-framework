cc1dc9b90f04217eadd3449aa676ad2ded5ad21d
Maja Frankiewicz <majafrank@live.se>
2023-03-22 09:23:37 +0100

    auto commit to b

--- api/routes.js
+++ api/routes.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const helpers = require("./helpers.js");
-require("dotenv").config();
 const { apiCallPost, apiCallGet, apiCallDelete } = require("./helpers");
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
+require("dotenv").config();
 
 router.post('/getListData', async (req, res) => {
 
-  console.log(req.body)
-  // const parentId = process.env[req.body.myBody];
+  const type = req.body.body
+  const url = process.env[type];
 
-  // let response = await apiCallGet(``);
+  let response = await apiCallGet(url);
 
-  // if ((await response.status) !== 200) {
-  //     return res.status(response.status).json(response.data);
-  // } else {
-  //     return res.json(response.data);
-  // }
-  res.json('Hello World!');
+  if ((await response.status) !== 200) {
+      return res.status(response.status).json(response.data);
+  } else {
+      return res.json(response.data);
+  }
+
+  // res.json('Hello World!')
+
 });
 
 module.exports = router;
