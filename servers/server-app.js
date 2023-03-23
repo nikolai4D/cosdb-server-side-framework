d1e35eef9bfd4f8df2b97579c45b1552bc924e6c
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-23 07:51:34 +0100

    auto commit to b

--- servers/server-app.js
+++ servers/server-app.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const fs = require("fs");
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-
 app.use(
   "/",
   express.static(
-    path.join(__dirname, "/../../../node_modules/cosdb-client-framework/app-boilerplate")
+    path.join(
+      __dirname,
+      "/../../../node_modules/cosdb-client-framework/app-boilerplate"
+    )
   )
 );
 
-app.get("/read", (req, res) => {
+app.get("/read/:key", (req, res) => {
+  const { key } = req.params;
   fs.readFile(
-    path.join(__dirname, "/../../../model.json"),
+    path.join(__dirname, `/../../../model/model_${key}.json`),
     "utf-8",
     (error, data) => {
       if (error) {
@@ -34,6 +36,20 @@ app.get("/read", (req, res) => {
   );
 });
 
+app.get("/read", (req, res) => {
+  fs.readFile(
+    path.join(__dirname, "/../../../model/model.json"),
+    "utf-8",
+    (error, data) => {
+      if (error) {
+        console.error("An error occurred while reading the file:", error);
+        res.sendStatus(500);
+      } else {
+        res.json(JSON.parse(data));
+      }
+    }
+  );
+});
 
 app.use(
   "/core",
@@ -41,7 +57,6 @@ app.use(
     path.join(__dirname, "/../../../node_modules/cosdb-client-framework/core")
   )
 );
-
 
 app.use(
   "/components",
@@ -53,7 +68,6 @@ app.use(
   )
 );
 
-
 app.use(
   "/data-mgmt",
   express.static(
@@ -64,17 +78,17 @@ app.use(
   )
 );
 
-
 // Api
 app.use("/api", require("../api/routes.js"));
 
-
-
-app.get('*', function(req, res, next) {
-    res.sendFile( path.join(__dirname, "/../../../node_modules/cosdb-client-framework/app-boilerplate/index.html")
-    );
-})
-
+app.get("*", function (req, res, next) {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "/../../../node_modules/cosdb-client-framework/app-boilerplate/index.html"
+    )
+  );
+});
 
 app.listen(3005, () => {
   console.log("server is listening on port 3005");
