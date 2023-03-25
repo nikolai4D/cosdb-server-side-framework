5a9511db865e07603d5d8b47c60212157f7c9313
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-25 17:34:15 +0100

    auto commit to b

--- servers/helpers/readModelFromParentId.js
+++ servers/helpers/readModelFromParentId.js
@@ -4,7 +4,7 @@ const path = require("path");
 async function readModelFromParentId(key, parentId) {
   const model = JSON.parse(
     fs.readFileSync(
-      path.join(__dirname, `/../../../model/model_${key}.json`),
+      path.join(__dirname, `/../../../../model/model_${key}.json`),
       "utf-8"
     )
   );
